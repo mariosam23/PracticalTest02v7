07151abcc758c217f140cf08c8055eedbd72297e
Sâmpetru D. Mario <119307188+mariosam23@users.noreply.github.com>
2026-01-21 14:12:32 +0200

    start test

---
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest02v7/PracticalTest02V7MainActivity.kt
@@ -0,0 +1,14 @@
+package ro.pub.cs.systems.eim.practicaltest02v7
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PracticalTest02V7MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_practical_test02v7_main)
+    }
+}
